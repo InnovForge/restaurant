f389f03e7561100c856dced0f1c76b06456196e4
Nguyen Thanh Nhat <nguyenthanhnhat3@dtu.edu.vn>
2025-01-18 13:37:07 +0700

    delete file and add file admin

--- apps/frontend/src/app/routes/app/admin/admin-restaurant.jsx
+++ apps/frontend/src/app/routes/app/admin/admin-restaurant.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const AdminRestaurant = () => {
   return (
     <div>
-        this is page Admin Restaurant
+        
     </div>
   )
 }
 
-export default AdminRestaurant
+export default admin-restaurant
