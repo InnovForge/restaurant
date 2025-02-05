64cc1281ac6b339f642b3755b5738af01b81c2ff
ngtuonghy <ngtuonghy@gmail.com>
2025-02-05 11:49:30 +0700

    fix remove auth in callback

--- apps/backend/src/routes/payment.js
+++ apps/backend/src/routes/payment.js
@@ -2,7 +2,6 @@ import { Router } from "express";
 import * as paymentController from "../controllers/payment.js";
 import { authenticateJWT } from "../middlewares/auth.js";
 const router = Router();
-router.use("/payments", authenticateJWT);
-router.post("/payments/momo", paymentController.createPaymentMomo);
+router.post("/payments/momo", authenticateJWT, paymentController.createPaymentMomo);
 router.post("/payments/momo/callback", paymentController.callbackPaymentMomo);
 export default router;
