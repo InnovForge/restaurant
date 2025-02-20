1d93a88832043e376ef28a45827b511200dab4ce
ngtuonghy <ngtuonghy@gmail.com>
2025-02-21 06:23:15 +0700

    change cn to utlis folder

--- apps/frontend/src/components/ui/card.jsx
+++ apps/frontend/src/components/ui/card.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/utils/cn";
 
 const Card = React.forwardRef(({ className, ...props }, ref) => (
   <div ref={ref} className={cn("rounded-xl border bg-card text-card-foreground shadow", className)} {...props} />
