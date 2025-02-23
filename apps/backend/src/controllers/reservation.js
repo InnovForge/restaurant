7ddd7b16c6eaa4d21c7eee205816084de3f1f721
Nguyen Thanh Nhat <nguyenthanhnhat3@dtu.edu.vn>
2025-02-23 21:19:53 +0700

    sửa revervation

--- apps/backend/src/controllers/reservation.js
+++ apps/backend/src/controllers/reservation.js
@@ -2,26 +2,54 @@ import reservationModel from "../models/reservation.js";
 import responseHandler, { ERROR_TYPE } from "../utils/response.js";
 import { validateFields } from "../utils/validate-fields.js";
 
-export const createReservation = async (req, res) => {
-  const { restaurant_id, user_id, table_number, reservation_status } = req.body;
+// export const createReservation = async (req, res) => {
+//   const { restaurantId, userId, tableNumber, reservationStatus } = req.body;
 
-  const errors = validateFields(req.body, ["restaurant_id", "user_id", "table_number", "reservation_status"], true);
+//   const errors = validateFields(req.body, ["restaurant_id", "user_id", "table_number", "reservation_status"], true);
+
+//   if (errors) {
+//     return responseHandler.badRequest(res, undefined, errors);
+//   }
+
+//   try {
+//     const reservationId = await reservationModel.createReservation({
+//       restaurantId,
+//       userId,
+//       tableNumber,
+//       reservationStatus,
+//     });
+
+//     return responseHandler.created(res, undefined, { reservationId });
+//   } catch (error) {
+//     console.log("error :>> ", error);
+//     return responseHandler.internalServerError(res);
+//   }
+// };
+
+export const createReservation = async (req, res) => {
+  const formattedBody = {
+    restaurant_id: req.body.restaurantId,
+    user_id: req.body.userId,
+    table_number: req.body.tableNumber,
+    reservation_status: req.body.reservationStatus,
+  };
+
+  const errors = validateFields(
+    formattedBody,
+    ["restaurant_id", "user_id", "table_number", "reservation_status"],
+    true,
+  );
 
   if (errors) {
     return responseHandler.badRequest(res, undefined, errors);
   }
 
   try {
-    const reservationId = await reservationModel.createReservation({
-      restaurant_id,
-      user_id,
-      table_number,
-      reservation_status,
-    });
+    const reservation = await reservationModel.createReservation(formattedBody);
 
-    return responseHandler.created(res, undefined, { reservationId });
+    return responseHandler.created(res, undefined, reservation);
   } catch (error) {
-    console.log("error :>> ", error);
+    console.log("Lỗi khi tạo đặt bàn: ", error);
     return responseHandler.internalServerError(res);
   }
 };
--- apps/backend/src/models/reservation.js
+++ apps/backend/src/models/reservation.js
@@ -1,16 +1,45 @@
 import { pool } from "../configs/mysql.js";
 import { nanoidNumbersOnly } from "../utils/nanoid.js";
 
+// const reservationModel = {
+//   async createReservation(reservationData) {
+//     const reservationId = nanoidNumbersOnly(16);
+//     const { restaurantId, userId, tableNumber, reservationStatus } = reservationData;
+
+//     try {
+//       const sql = `
+//             INSERT INTO reservations (reservation_id, restaurant_id, user_id, table_number, reservation_status)
+//             VALUES (?, ?, ?, ?, ? )
+//         `;
+
+//       const [result] = await pool.execute(sql, [
+//         reservationId,
+//         restaurantId,
+//         userId,
+//         tableNumber,
+//         reservationStatus,
+//       ]);
+
+//       if (result.affectedRows === 1) {
+//         return { success: true, reservationId };
+//       }
+//       return { success: false, message: "Failed to create reservation" };
+//     } catch (error) {
+//       console.error("Error creating reservation:", error);
+//       return { success: false, message: error.message };
+//     }
+//   },
+// };
+
 const reservationModel = {
-  async createReservation(reservationData) {
+  async createReservation({ restaurant_id, user_id, table_number, reservation_status }) {
     const reservationId = nanoidNumbersOnly(16);
-    const { restaurant_id, user_id, table_number, reservation_status } = reservationData;
 
     try {
       const sql = `
-            INSERT INTO reservations (reservation_id, restaurant_id, user_id, table_number, reservation_status)
-            VALUES (?, ?, ?,?,?)
-        `;
+        INSERT INTO reservations (reservation_id, restaurant_id, user_id, table_number, reservation_status)
+        VALUES (?, ?, ?, ?, ?)
+      `;
 
       const [result] = await pool.execute(sql, [
         reservationId,
@@ -23,9 +52,9 @@ const reservationModel = {
       if (result.affectedRows === 1) {
         return { success: true, reservationId };
       }
-      return { success: false, message: "Failed to create reservation" };
+      return { success: false, message: "Không thể tạo đặt bàn" };
     } catch (error) {
-      console.error("Error creating reservation:", error);
+      console.error("Lỗi khi tạo đặt bàn:", error);
       return { success: false, message: error.message };
     }
   },
