e3d039d51a112c805b2fba0204fe28ee0ca55b51
ngtuonghy <ngtuonghy@gmail.com>
2025-03-03 13:28:52 +0700

    contine: add checkout

--- apps/frontend/src/app/routes/app/checkout.jsx
+++ apps/frontend/src/app/routes/app/checkout.jsx
@@ -1,7 +1,83 @@
+import useCartStore from "@/stores/useCartStore";
+import momoLogo from "@/assets/svg/momo_square_pinkbg.svg";
+
+import { Card } from "@/components/ui/card";
+
+import { Label } from "@/components/ui/label";
+
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import LazyImage from "@/components/ui/lazy-image";
+import { Plus } from "lucide-react";
+import { Minus } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import SearchLocation from "@/features/address/components/search-location";
+import { useState } from "react";
+
 const Checkout = () => {
+  const { Cart, addCart } = useCartStore();
+  const [value, setValue] = useState("test");
+  console.log(value);
   return (
-    <div>
-      <h1>Checkout</h1>
+    <div className="flex-col flex w-full max-w-3xl gap-4 min-h-screen mx-auto">
+      <SearchLocation value={value} onChange={setValue} />
+      <div className="w-full flex flex-col gap-1">
+        {Cart.map((item, i) => (
+          <Card className="flex gap-2 overflow-hidden" key={i}>
+            <div className="w-28">
+              <AspectRatio ratio={4 / 3}>
+                <LazyImage
+                  width="100%"
+                  height="100%"
+                  src={item.foodImage}
+                  alt={item.foodName}
+                  className="object-cover rounded-xl"
+                />
+              </AspectRatio>
+            </div>
+            <div className="flex flex-col gap-2">
+              <p>{item.foodName}</p>
+              <div className="flex items-center gap-2">
+                <Plus />
+                <p className="text-md">{item.quantity}</p>
+                <Minus />
+              </div>
+            </div>
+          </Card>
+        ))}
+      </div>
+
+      {/* <div className="p-4 h-[500px] bg-background fixed w-[28%] right-20 top-24 transform"> */}
+      <Card className="p-2 h-full flex flex-col">
+        <h3>Chọn thanh toán</h3>
+
+        <Select defaultValue="momo">
+          <SelectTrigger className="w-full h-fit">
+            <SelectValue placeholder="payment" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="momo">
+              <div className="flex items-center justify-center gap-2">
+                <img src={momoLogo} alt="momo" className="w-10 h-10" />
+                <Label>Thanh toán qua Momo</Label>
+              </div>
+            </SelectItem>
+            <SelectItem value="later">
+              <Label>Thanh toán sau</Label>
+            </SelectItem>
+          </SelectContent>
+        </Select>
+
+        <div className="w-full mt-auto">
+          <h2 className="text-xl font-bold">Tổng cộng</h2>
+          <p>{Cart.reduce((acc, item) => acc + item.quantity, 0)} món</p>
+          <p>{Cart.reduce((acc, item) => acc + item.quantity * item.price, 0)} đ</p>
+
+          <Button className="w-full">Thanh toán</Button>
+        </div>
+      </Card>
+      {/* </div> */}
     </div>
   );
 };
